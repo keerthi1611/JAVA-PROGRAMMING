90e38d63f528430df55dadb35735e0bf7f6216dc
keerthi1611 <34644854+keerthi1611@users.noreply.github.com>
2017-12-24 16:11:11 +0530

    Create Swappingtemp.java

---
+++ ASSIGNMENT/Swappingtemp.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Swapping {
+	public static void main(String[] args)
+	{
+	Scanner sc=new Scanner(System.in);
+	int x=sc.nextInt();
+	int y=sc.nextInt();
+	int temp;
+	temp=x;
+	x=y;
+	y=temp;
+	System.out.println(x);
+	System.out.println(y);
+
+}}
