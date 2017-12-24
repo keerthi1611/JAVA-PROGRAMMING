a8ece484b0e3222bc4a400f4149c6903353b0d04
keerthi1611 <34644854+keerthi1611@users.noreply.github.com>
2017-12-24 15:09:14 +0530

    Create power.java

---
+++ ASSIGNMENT/power.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class Power {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int n,p,i,x=1;
+		n=sc.nextInt();
+		p=sc.nextInt();
+		if(n>1&&p==0)
+		{
+			System.out.println("1");
+		}
+		else if(n>1&&p==1)
+		{
+			System.out.println("0");
+		}
+		else
+		{
+			for(i=1;i<=p;i++)
+			{
+				x=n*x;
+			}
+			System.out.println(x);
+		}
+	}
+}
