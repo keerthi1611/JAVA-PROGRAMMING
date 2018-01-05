c74a059b907de189a49732a0e7d44121d1ba352c
keerthi1611 <34644854+keerthi1611@users.noreply.github.com>
2018-01-05 22:18:52 +0530

    Create countvowels.java

---
+++ ASSIGNMENT/countvowels.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class sample
+{
+public static void main(String args[])
+{
+    String inputStr;
+        int v = 0;
+        int n = 0;
+        Scanner sc = new Scanner(System.in);
+        String str;
+       str=sc.nextLine();
+       Character c;
+       n=str.length();
+        for (int i = 0; i <n; i++) {
+            c = str.charAt(i);
+            if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' || c == 'a' || c == 'e' || c == 'i' || c ==
+                    'o' || c == 'u') {
+                v++;
+            }
+        }
+        System.out.println("No of vowels:"+v);
+        
+    }
+}
